6cc30879dc388daa3590172a54ef528f6b00ec80
Matthias Barde <mbarde@uni-koblenz.de>
2023-10-19 17:09:37 +0200

    add button to copy qr code URL

--- static/main.js
+++ static/main.js
@@ -4,6 +4,7 @@ function createQRCode(event) {
         document.getElementById('qrcode').src = '';
         document.getElementById('qrcode').style.display = 'none';
         document.getElementById('btn-download').style.display = 'none';
+        document.getElementById('btn-copy').style.display = 'none';
         event.preventDefault();
         return false;
     }
@@ -31,6 +32,28 @@ function createQRCode(event) {
     btnDownload.href = src;
     btnDownload.style.display = 'block';
 
+    const btnCopy = document.getElementById('btn-copy');
+    btnCopy.onclick = () => {
+        text2clip(window.location.href + src, btnCopy);
+    }
+    btnCopy.style.display = 'block';
+
     event.preventDefault();
     return false;
+}
+
+function text2clip(text, el) {
+    let dummy = document.createElement('input');
+    dummy.value = text;
+    document.body.appendChild(dummy);
+    dummy.select();
+    dummy.setSelectionRange(0, 99999); /* mobile devices */
+    document.execCommand('copy');
+    dummy.remove();
+    if (el !== null) {
+      el.classList.add('shake');
+      setTimeout(function() {
+        el.classList.remove('shake');
+      }, 500);
+    }
 }
