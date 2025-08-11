6e8204a3187371b60ed1a19ff06d5cd5df5ae628
Matthias Barde <mbarde@uni-koblenz.de>
2025-08-11 12:38:33 +0200

    fix: remove useless file

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-path = '/home/mbarde/projects/flask-qrcode/codes/'
-sizeInBytes = sum(os.path.getsize(path + f) for f in os.listdir(path) if os.path.isfile(path + f))
-sizeInMB = sizeInBytes / 1000000
-print(sizeInMB)
